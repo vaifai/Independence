6627e5a4f32c3390f47d5187fb15d9f41b76c8c3
vaifai <vaifaipandey1996@gmail.com>
2022-07-24 17:49:33 +0530

    Initial changes for UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Header from "./Header";
+import './App.css';
+import NewsFeed from "./NewsFeed";
+import Stats from "./Stats";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello from independence</h1>
+      {/*Header*/}
+      <div className="app__header">
+        <Header/>
+      </div>
+
+      <div className="app__body">
+        <div className="app__container"></div>
+          <NewsFeed/>
+          
+          <Stats/>
+      </div>
     </div>
   );
 }
---
+++ src/NewsFeed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewsFeed.css';
+
+function NewsFeed() {
+  return (
+    <div className='newsfeed'>
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default NewsFeed;
